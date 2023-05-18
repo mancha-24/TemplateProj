06bda28c3cca1b0098c71a1de39aae27aab0e274
Manuel Chavez <manuel1995_01@outlook.com>
2023-05-18 11:29:34 -0500

    adding router

---
+++ client-app/src/app/router/Routes.tsx
@@ -0,0 +1,11 @@
+import { type RouteObject, createBrowserRouter } from 'react-router-dom'
+import App from '../../App'
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />
+  }
+]
+
+export const router = createBrowserRouter(routes)
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
